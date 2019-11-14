14ee171ea74223f4a7c81287f396adca309af28c
ethanhodess <53194842+ethanhodess@users.noreply.github.com>
2019-11-14 10:44:59 -0800

    fish

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// Javascript Canvas Intro!
+
+/**
+ *  This is called once after the HTML of the page loads
+ *
+ */
+function Start() {
+
+  // Initialize Spaceship
+  InitializeSpaceship();
+}
